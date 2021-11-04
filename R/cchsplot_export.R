b9a0b46ab0d4ee5f11fb3e521de41bdbadf49810
CarlosMunozDiazCSIC <carlos.munoz@cchs.csic.es>
2021-11-04 13:27:00 +0100

    Prueba en exportación

--- R/cchsplot_export.R
+++ R/cchsplot_export.R
@@ -17,6 +17,8 @@ create_footer <- function (source_name, logo_image_path) {
     footer <- grid::grobTree(grid::linesGrob(x = grid::unit(c(0, 1), "npc"), y = grid::unit(1.125, "npc")),
                              grid::textGrob(source_name, x = 0.005, hjust = 0, gp = grid::gpar(fontsize=11)))
   } else {
+    logo_image_path <- file.path(system.file("img", package = 'cchsplot'),"cchs.png")
+
     footer <- grid::grobTree(grid::linesGrob(x = grid::unit(c(0, 1), "npc"), y = grid::unit(1.125, "npc")),
                              grid::textGrob(source_name, x = 0.005, hjust = 0, gp = grid::gpar(fontsize=11)),
                              grid::rasterGrob(png::readPNG(logo_image_path), x = 0.97))
