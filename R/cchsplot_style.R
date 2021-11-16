e2cfa765b32745671ecf22b4093ce17263d90ccd
CarlosMunozDiazCSIC <carlos.munoz@cchs.csic.es>
2021-11-16 09:41:09 +0100

    Cambio en estilos y exportación

--- R/cchsplot_style.R
+++ R/cchsplot_style.R
@@ -1,5 +1,5 @@
 cchsplot_style <- function(){
-  font <- "Georgia"   #assign font family up front
+  font <- "Helvetica"   #assign font family up front
 
   ggplot2::theme(
       #Elementos genéricos
@@ -10,15 +10,13 @@ cchsplot_style <- function(){
 
       ##Líneas ejes
       panel.grid.major.y = ggplot2::element_line(
-        size = 0.25,
+        size = 0.15,
         color = '#262626'
       ),
 
       ##Elementos propios de la visualización > Textos
-      plot.title = ggplot2::element_blank(),               #raise slightly
-
+      plot.title = ggplot2::element_blank(),             #raise slightly
       plot.subtitle = ggplot2::element_blank(),               #font size
-
       plot.caption = ggplot2::element_blank(),               #right align
 
       axis.title = ggplot2::element_text(             #axis titles
@@ -28,6 +26,10 @@ cchsplot_style <- function(){
       axis.text = ggplot2::element_text(              #axis text
         family = font,            #axis famuly
         size = 10),                #font size
+
+      axis.text.y = ggplot2::element_text(
+        margin=ggplot2::margin(-10, b = 5)
+      ),
 
       axis.text.x = ggplot2::element_text(            #margin for axis text
         margin=ggplot2::margin(-15, b = 7.5)),
