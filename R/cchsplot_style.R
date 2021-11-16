d3993b4a5300fae9e60e761db156118ed5489448
CarlosMunozDiazCSIC <carlos.munoz@cchs.csic.es>
2021-11-16 09:50:57 +0100

    Ajustes en estilos

--- R/cchsplot_style.R
+++ R/cchsplot_style.R
@@ -11,27 +11,27 @@ cchsplot_style <- function(){
       ##Líneas ejes
       panel.grid.major.y = ggplot2::element_line(
         size = 0.15,
-        color = '#262626'
+        color = 'grey96'
       ),
 
       ##Elementos propios de la visualización > Textos
-      plot.title = ggplot2::element_blank(),             #raise slightly
-      plot.subtitle = ggplot2::element_blank(),               #font size
-      plot.caption = ggplot2::element_blank(),               #right align
+      plot.title = ggplot2::element_blank(),
+      plot.subtitle = ggplot2::element_blank(),
+      plot.caption = ggplot2::element_blank(),
 
-      axis.title = ggplot2::element_text(             #axis titles
+      axis.title = ggplot2::element_text(
         family = font,            #font family
-        size = 11),               #font size
+        size = 14),               #font size
 
-      axis.text = ggplot2::element_text(              #axis text
+      axis.text = ggplot2::element_text(
         family = font,            #axis famuly
-        size = 10),                #font size
+        size = 12),                #font size
 
       axis.text.y = ggplot2::element_text(
         margin=ggplot2::margin(-10, b = 5)
       ),
 
-      axis.text.x = ggplot2::element_text(            #margin for axis text
+      axis.text.x = ggplot2::element_text(
         margin=ggplot2::margin(-15, b = 7.5)),
 
       ##Leyenda
